a8c3e1262204daddc0c2e57a4b804b09742576bb
ajay jairath <gjairath@purdue.edu>
2020-07-26 01:56:10 -0400

    Added segment tree

--- segment.cpp
+++ segment.cpp
@@ -9,27 +9,39 @@ using namespace std;
 
 typedef long long int ll;
 
-int* build_tree (int index, int arr[], int start, int end) {
-	int* st;
+int build_tree (int index, int arr[], int* st, int start, int end) {
+//	int* st;
 	
 	if (start == end) {
 		st[index] = arr[start];	
-		return;
+		return arr[start];
 	}
 	int mid = (start + end) / 2;
-	build(2*index + 1, arr, start, mid);
-	build(2*index + 2, arr, mid + 1, end);
 	// Once the recursion is complete, we;ve hit the bottom of the tree so
-	st[index] = (st[2*index + 1] + st[2*index +2]);
+	st[index] = build_tree(2*index + 1, arr, st, start, mid) + build_tree(2*index + 2, arr, st, mid + 1, end);
+}
+
+int* reserve_memory(int arr[], int n, int* height) {
+	// 2 * 2 ^ log2 N - 1 
 	
+	int exp = (int) log2(n);
+	*height = 2 * pow(2, exp) - 1;
+	int* seg = new int[*height];
+	build_tree(0, arr, seg, 0, n - 1);
+	return seg;
 }
 
 int main() {
+
+	int arr[] = {3, 1, 2, 5, 6, 8, 3, 2};  
+    int n = sizeof(arr) / sizeof(arr[0]);
+	int size = 1;  
+	int* seg = reserve_memory(arr, n, &size);
 	
-	int arr[4] = {3, 1, 2, 5, 6, 8, 3, 2};
-	
-	int sts[]
-	
+	cout << size << endl;
+	for (int i = 0; i < 15; i++) {
+		cout << seg[i] << ' ';
+	}
 	
 	return 0; 
 }
