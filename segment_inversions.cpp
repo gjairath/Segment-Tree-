91746182c9ce481e091f6ee8db2282f8bfef0f91
ajay jairath <gjairath@purdue.edu>
2020-08-10 01:00:44 -0400

    Inversion ongoing

--- segment_inversions.cpp
+++ segment_inversions.cpp
@@ -12,38 +12,4 @@ using namespace std;
 
 typedef long long int ll;
 
-void debug_printer(int size, int*seg) {
-	cout << endl;
-	for (int i = 0; i < size; i++) {
-		cout << seg[i] << ' ';
-	}
-	cout << endl;	
-}
 
-void update(int* seg, int index_up, int nn) {
-	
-	int c = 0;
-	
-	if (seg[index_up] == 0) {
-		for (int i = index_up; i < nn; i++) {
-			c += seg[i];
-		}
-		if (c == 0) {
-			c = 1;
-		}
-	seg[index_up] = c;
-	return;
-	}
-}
-
-int* reserve_memory_max(int arr[], int n) {
-	// 2 * 2 ^ log2 N - 1 
-
-	
-	int* seg = new int[n];
-	for (int i = 0; i < n; i ++) {
-		seg[i] = 0;
-	}
-	return seg;
-	
-}
--- test_segment_inversions.cpp
+++ test_segment_inversions.cpp
@@ -1,38 +1,18 @@
 #include "segment_inversions.cpp"
 
-static int find_index(int item, int arr[], int nn) {
-	
-	for (int i = 0; i < nn; i++){
-		if (item == arr[i]) {
-			return i;
-		}
-	}
-}
+
 
 int main() {
 //	int arr[5] = {20, 40, 1, 2, 5};
 	int arr[5] = {4, 1, 3, 5, 2};
 	int nn = sizeof(arr) / sizeof(arr[0]);
 
-	int* seg = reserve_memory_max(arr, nn);
- 	
-	debug_printer(nn, seg);	
 
-	int arr2[5];
+	int max = *max_element(arr, arr + nn);
 	
-	for (int i = 0; i < nn; i++){
-		arr2[i] = arr[i];
-	}	
-	sort(arr2, arr2 + nn);
+	int* seg = new int[6 * max];
+	memset(seg, 0, 6*max);
 	
-	
-	for (int i = 0; i < nn; i++) {
-		
-		update(seg, find_index(arr[i], arr2, nn), nn);
-	}
-	
-	cout << endl;
-	debug_printer(nn, seg);
 	
 	return 0; 
 }
